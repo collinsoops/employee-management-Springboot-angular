cd0bdbe5f193e7181c37490d64bd7b51a1579a73
COLLINS <ccollins2030cc@gmail.com>
2021-09-22 15:57:26 +0300

    first commit

---
+++ src/main/java/tech/arrays/employeemanager/model/Employee.java
@@ -0,0 +1,89 @@
+package tech.arrays.employeemanager.model;
+
+import javax.persistence.*;
+import java.io.Serializable;
+@Entity
+public class Employee implements Serializable {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(nullable = false,updatable = false)
+    private Long id;
+    private String name;
+    private String email;
+    private String jobTitle;
+    private String phone;
+    private String imageurl;
+    @Column(nullable = false,updatable = false)
+    private String employeecode;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getJobTitle() {
+        return jobTitle;
+    }
+
+    public void setJobTitle(String jobTitle) {
+        this.jobTitle = jobTitle;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    public String getImageurl() {
+        return imageurl;
+    }
+
+    public void setImageurl(String imageurl) {
+        this.imageurl = imageurl;
+    }
+
+    public String getEmployeecode() {
+        return employeecode;
+    }
+
+    public void setEmployeecode(String employeecode) {
+        this.employeecode = employeecode;
+    }
+
+    @Override
+    public String toString() {
+        return "Empoyee {" +
+                "id" + id +
+                "name" + name +
+                "email" + email +
+                "jobtitle" + jobTitle +
+                "phone" + phone +
+                "imageurl" + imageurl+
+                "}";
+    }
+
+    }
+
+
